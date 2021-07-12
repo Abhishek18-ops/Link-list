93c7b2225ef56a74e50febd33afcbfcaf5662262
Abhishek Maurya <abhisheaurya@gmail.com>
2021-07-12 09:08:23 +0530

    initial commit

--- insertion.cpp
+++ insertion.cpp
@@ -11,3 +11,40 @@ class Node{
     }
 };
 
+void insertatTail(Node* &head,int val){
+    Node* n = new Node(val);
+    Node* temp = head;
+
+    if(head == NULL){
+        head = n;
+        return ;
+    }
+    while(temp->next!=NULL){
+        temp = temp->next;
+
+    }
+    temp->next = n;
+
+}
+void display(Node* head){
+    Node*temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    Node* head = NULL;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+        insertatTail(head,arr[i]);
+    }
+    display(head);
+
+}
+
